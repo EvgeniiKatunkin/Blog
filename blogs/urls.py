58e89caff1501c425ad05431749b5d63fb72734e
EvgeniiKatunkin <evgeniikatunkin@gmail.com>
2021-05-26 17:37:45 +0500

    I need to fix the error, than it's done.

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,4 +7,10 @@ from . import views
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+
+    # Page for adding a new post
+    path('new_post/', views.new_post, name='new_post'),
+
+    # Page for editing existing posts
+    path('edit_post/', views.edit_post, name='edit_post'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 from .models import BlogPost
+from .forms import PostForm
 
 
 def index(request):
@@ -7,3 +11,39 @@ def index(request):
     posts = BlogPost.objects.order_by('-date_added')
     context = {'posts': posts}
     return render(request, 'blogs/index.html', context)
+
+
+def new_post(request):
+    """Add a new post."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = PostForm()
+    else:
+        # POST data submitted; process data.
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('blogs:index'))
+
+    context = {'form': form}
+    return render(request, 'blogs/new_post.html', context)
+
+
+def edit_post(request, post_id):
+    """Edit an existing post."""
+    post = BlogPost.objects.get(id=post_id)
+    title = post.title
+    text = post.text
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current post.
+        form = BlogPost(instance=post)
+    else:
+        # POST data submitted; process data.
+        form = BlogPost(instance=post, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('blogs:index'))
+
+    context = {'title': title, 'text': text, 'form': form, 'post': post}
+    return render(request, 'blogs/edit_post.html', context)
